8d1efe8f79d40b40f6f57c0d268c29fd758cff07
saswati <saswati@Ubuntu22.04.myguest.virtualbox.org>
2023-03-28 14:59:27 +0530

    nbit addition

---
+++ adder/nbitfa/nbitfa.cpp
@@ -0,0 +1,48 @@
+#include "systemc.h"
+#include "fa_ha.cpp"
+#include "const.h"
+
+SC_MODULE(FullAdder16) {
+    sc_in<sc_bv<1>> a[N];
+    sc_in<sc_bv<1>> b[N];
+    sc_in<sc_bv<1>> c_in;
+    sc_out<sc_bv<1>> sum[N];
+    sc_out<sc_bv<1>> c_out;
+    
+    sc_in<bool> clk, reset;
+
+    FA* fa[N];
+    sc_signal<sc_bv<1>> c[N];
+
+    SC_CTOR(FullAdder16) {
+        // Instantiate 16 1-bit Full Adder modules
+        for (int i = 0; i < N; i++) {
+            fa[i] = new FA(sc_gen_unique_name("fa"));
+            fa[i]->a(a[i]);
+            fa[i]->b(b[i]);
+            //fa[i]->c_in(c[i]);
+            
+            if (i == 0) {
+                fa[i]->c_in(c_in);
+            }
+            else {
+                fa[i]->c_in(c[i]);
+		//fa[i]->c_in(fa[i-1]->c_in);
+            }
+	    fa[i]->sum(sum[i]);
+	    if(i==N-1){
+		    fa[i]->c_out(c_out);
+	    }
+	    else {
+		    fa[i]->c_out(c[i+1]);
+		    //fa[i]->c_out(fa[i+1]->c_out);
+	    }
+	    fa[i]->clk(clk);
+	    fa[i]->reset(reset);
+
+
+        }
+
+        
+    }
+};
---
+++ adder/nbitfa/nbitfa_tb.cpp
@@ -0,0 +1,204 @@
+#include "systemc.h"
+#include "nbitfa.cpp"
+
+
+SC_MODULE(nbitfa_tb){
+
+    sc_clock clk;
+    sc_signal<bool> reset;
+    sc_signal<sc_bv<1>> a[N], b[N], sum[N], c_in;
+    sc_signal<sc_bv<1>> c_out;
+
+    // Instantiate the N-bit adder
+    //sc_trace_file *pTracefile;
+    sc_trace_file *tf;
+
+    FullAdder16* adder;
+
+
+    void stimulus();
+
+
+    SC_CTOR(nbitfa_tb):
+	    clk("clk", 10, SC_NS)
+	{
+	    // Connect the signals
+	    adder = new FullAdder16("adder");
+
+            adder->clk(clk);
+            adder->reset(reset);
+
+	    for(int i = 0; i< N; i++){
+		    adder->a[i](a[i]);
+            	    adder->b[i](b[i]);
+            	    adder->sum[i](sum[i]);
+	    }
+
+	    adder->c_in(c_in);
+	    adder->c_out(c_out);
+
+	   
+
+            //Open VCD file
+           /*pTracefile = sc_create_vcd_trace_file("waveforms");
+	    
+            sc_trace(pTracefile, clk, "clk");
+            sc_trace(pTracefile, reset, "reset");
+	    for(int i = 0; i< N; i++){
+                sc_trace(pTracefile, a[i], "a"+ std::to_string(i));
+                sc_trace(pTracefile, b[i], "b"+ std::to_string(i));
+                sc_trace(pTracefile, sum[i], "sum"+ std::to_string(i));
+	    }
+            sc_trace(pTracefile, c_in, "c_in");
+            sc_trace(pTracefile, c_out, "c_out");*/
+
+	    SC_THREAD(stimulus);
+    }
+
+    ~nbitfa_tb(){
+	    sc_close_vcd_trace_file(tf);
+	    delete adder;
+	    
+    }
+};
+
+
+void nbitfa_tb::stimulus(){
+	
+	srand(time(NULL));
+	while (true){
+		cout << "start" <<endl;
+
+		reset.write(rand() % 2);
+		cout << " reset = " << reset.read() << endl;
+		
+		for(int i = 0; i<N;i++){
+			a[i].write(rand() % 2);
+		        b[i].write(rand() % 2);
+		}
+	        c_in = rand() % 2;
+
+		cout << "a = ";
+		for(int i = N-1; i>=0 ; i--){
+			cout << a[i];
+		}
+		cout << "  ";
+
+		cout << "b = ";
+		for(int i = N-1; i>=0 ; i--){
+			cout << b[i];
+		}
+		cout << "  ";
+		cout << " c_in = " << c_in.read() << endl;
+
+		cout << "sum = ";
+		for(int i = N-1; i>=0 ; i--){
+			cout << sum[i];
+		}
+		cout << " ";
+
+		cout << " c_out = " << c_out.read() << endl;
+	
+		cout << "end " << endl;
+                wait(0.01, SC_NS);   //added this because of in the wave form i am geting started from the 00 which is not needed but when reset is 1 is taking some extra datas
+
+
+		// Combine signals for tracing
+		sc_bv<N> a_combined, b_combined, sum_combined;
+		// Update combined signals
+		
+		for (int i = 0; i < N; i++) {
+                a_combined[i] = a[i].read().get_bit(0);
+                b_combined[i] = b[i].read().get_bit(0);
+                sum_combined[i] = sum[i].read().get_bit(0);
+		
+		}
+
+		tf = sc_create_vcd_trace_file("waveform");
+
+		// Add signals to trace
+		sc_trace(tf, clk, "clk");
+	       	sc_trace(tf, reset, "reset");
+		sc_trace(tf, c_in, "c_in");
+	       	sc_trace(tf, c_out, "c_out");
+		
+		// Trace combined signals
+		sc_trace(tf, a_combined, "a_combined");
+	       	sc_trace(tf, b_combined, "b_combined");
+	       	sc_trace(tf, sum_combined, "sum_combined");
+
+
+		wait(10, SC_NS);
+	}
+}
+
+
+int sc_main(int argc, char* argv[]){
+       nbitfa_tb test("test");
+       sc_start(100, SC_NS);
+       sc_stop();
+       return 0;
+} 
+
+
+/*int sc_main(int argc, char* argv[]) {
+    sc_signal<sc_bv<1>> a[N];
+    sc_signal<sc_bv<1>> b[N];
+    sc_signal<sc_bv<1>> c_in;
+    sc_signal<sc_bv<1>> sum[N];
+    sc_signal<sc_bv<1>> c_out;
+    sc_signal<bool> clk, reset;
+
+    // Initialize inputs
+    //a.write(1010101010101010);
+    //b[0] = 
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++) {
+	    a[i].write(rand() % 2);
+	    b[i].write(rand() % 2);
+    }
+    c_in = rand() % 2;
+
+    // Instantiate FullAdder16 module
+    FullAdder16 fulladder("fulladder");
+    for (int i = 0; i < N; i++) {
+        fulladder.a[i](a[i]);
+        fulladder.b[i](b[i]);
+        fulladder.sum[i](sum[i]);
+    }
+    fulladder.c_in(c_in);
+    fulladder.c_out(c_out);
+    fulladder.clk(clk);
+    fulladder.reset(reset);
+
+
+    // Simulate for 10 ns
+    sc_start(10, SC_NS);
+
+    // Change inputs and continue simulating for another 10 ns
+    //c_in = 1;
+    //sc_start(10, SC_NS);
+
+    // Print out results
+    cout << "Inputs: a = ";
+    for (int i = N-1; i >= 0; i--) {
+        cout << a[i];
+    }
+    cout << ", b = ";
+    for (int i = N-1; i >= 0; i--) {
+        cout << b[i];
+    }
+    cout << ", c_in = " << c_in << std::endl;
+    cout << "Outputs: sum = ";
+    for (int i = N-1; i >= 0; i--) {
+        cout << sum[i];
+    }
+    cout << ", c_out = " << c_out << std::endl;
+
+    return 0;
+}
+*/
+
+
+
